63b8498d25d294e420e5cc4ad50bd393b502dcdb
yankfy <yankfy@qq.com>
2018-01-24 22:48:10 +0800

    koa post接收

--- post.js
+++ post.js
@@ -21,15 +21,45 @@ app.use(async(ctx) => {
         `
         ctx.body = html
     } else if (ctx.url === '/' && ctx.method === "POST") {
-        ctx.body = '接收到POST参数'
+        // ctx.body = '接收到POST参数'
+        // 利用await来接收
+        let postData = await parsePostData(ctx)
+        // console.log(postData)
+        ctx.body = postData
     } else {
         ctx.body = '<h1>404</h1>'
     }
-
-    // ctx.body = {
-    //     ctx
-    // }
 })
+
+let parsePostData = (ctx) => {
+    return new Promise((reserve, reject) => {
+        try {
+            let postData = ''
+            ctx.req.addListener("data", (data) => {
+                postData += data
+            })
+            ctx.req.on('end', () => {
+                let parseData = parseQueryStr(postData)
+                reserve(parseData)
+            })
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+let parseQueryStr = (queryStr) => {
+    let queryData = {}
+    let queryList = queryStr.split("&")
+    // queryList.entries() 返回一个带索引的数组
+    for (let [index, queryStr] of queryList.entries()) {
+        let itemList = queryStr.split("=")
+        console.log(itemList)
+        queryData[itemList[0]] = decodeURIComponent(itemList[1])
+        console.log(queryData)
+    }
+    return queryData
+}
 
 app.listen(3001, () => {
     console.log("3001端口已开")
