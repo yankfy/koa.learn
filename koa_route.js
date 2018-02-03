3a5bb17460a7c07b8a2e529d582d9e38b516c538
yankfy <yankfy@qq.com>
2018-02-03 20:58:04 +0800

    koa koa_route路由层级

--- koa_route.js
+++ koa_route.js
@@ -6,18 +6,40 @@ const app = new Koa()
 
 // const router = new Router()
 // 将所有路由前面添加一个统一的层级
-const router = new Router({
-    prefix:"/public"
+// const router = new Router({
+//     prefix:"/public"
+// })
+
+// router.get("/", (ctx, next) => {
+//     ctx.body = "Hello Pony"
+// }).get("/todo",(ctx,next)=>{
+//     ctx.body = "Hello Todo"
+// })
+
+let home = new Router();
+home.get('/pony', async (ctx) => {
+    ctx.body = "Home Pony";
+}).get('/todo', async (ctx) => {
+    ctx.body = 'Home ToDo';
 })
 
-router.get("/", (ctx, next) => {
-    ctx.body = "Hello Pony"
-}).get("/todo",(ctx,next)=>{
-    ctx.body = "Hello Todo"
+let page = new Router();
+page.get('/pony', async (ctx) => {
+    ctx.body = "Page Pony";
+}).get('/todo', async (ctx) => {
+    ctx.body = 'Page ToDo';
 })
+
+//装载所有子路由
+let router = new Router()
+router.use('/home', home.routes(), home.allowedMethods())
+router.use('/page', page.routes(), page.allowedMethods())
+
+app.use(router.routes).use(router.allowedMethods())
 
 // 调用router 并遵循请求方式
-app.use(router.routes()).use(router.allowedMethods())
+// 加载路由中间件
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen("3000", () => {
     console.log("3000 prot is open")
